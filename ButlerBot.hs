6552e80931fa8ce5098e954d1bfa23b2a4f0fab7
Wilfred Hughes <me@wilfred.me.uk>
2013-05-27 15:39:21 +0100

    defining locations and adding london as a location

--- ButlerBot.hs
+++ ButlerBot.hs
@@ -1,3 +1,12 @@
 
+newtype Latitude = Latitude Double deriving Show
+newtype Longitude = Longitude Double deriving Show
+
+data Location = Location (Latitude, Longitude)
+                deriving Show
+
+london :: Location
+london = Location (Latitude 51.5265, Longitude 0.0825)
+
 
 main = putStrLn "Hello, World!"

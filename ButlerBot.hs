8382ab6439517b7b77621157a6a1f1434f8b9559
Wilfred Hughes <me@wilfred.me.uk>
2013-05-27 16:54:41 +0100

    parsing json

--- ButlerBot.hs
+++ ButlerBot.hs
@@ -1,4 +1,5 @@
 import Network.Curl
+import Text.JSON
 
 newtype Latitude = Latitude Double deriving Show
 newtype Longitude = Longitude Double deriving Show
@@ -11,5 +12,18 @@ london = Location (Latitude 51.5265, Longitude 0.0825)
 
 httpGet :: URLString -> IO (CurlCode, String)
 httpGet url = curlGetString url []
+
+-- get JSON, returning Right for any failures
+-- based on http://www.amateurtopologist.com/blog/2010/11/05/a-haskell-newbies-guide-to-text-json/
+httpGetJson :: JSON a => URLString -> IO (Maybe a)
+httpGetJson url = do
+  (code, result) <- httpGet url
+  case (decode result) of
+    Ok object -> return (Just object)
+    Error s -> return Nothing
+
+-- assume that our URL returns an object at the top level
+httpGetJsonObject :: URLString -> IO (Maybe (JSObject JSValue))
+httpGetJsonObject url = httpGetJson url
 
 main = putStrLn "Hello, World!"

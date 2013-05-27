89561c308a4d73dfc2ecbf5a7d25ea4ac0433a1e
Wilfred Hughes <me@wilfred.me.uk>
2013-05-27 16:14:06 +0100

    adding an HTTP GET function

--- ButlerBot.hs
+++ ButlerBot.hs
@@ -1,3 +1,4 @@
+import Network.Curl
 
 newtype Latitude = Latitude Double deriving Show
 newtype Longitude = Longitude Double deriving Show
@@ -8,5 +9,7 @@ data Location = Location (Latitude, Longitude)
 london :: Location
 london = Location (Latitude 51.5265, Longitude 0.0825)
 
+httpGet :: URLString -> IO (CurlCode, String)
+httpGet url = curlGetString url []
 
 main = putStrLn "Hello, World!"

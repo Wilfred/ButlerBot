76d2aca2113134bc966b4be1bf51e04ee97fbbf0
Wilfred Hughes <me@wilfred.me.uk>
2013-08-03 12:24:43 +0100

    moving json code out into a separate file

--- ButlerBot.hs
+++ ButlerBot.hs
@@ -1,15 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Numeric
-import Network.Curl
 import Network.Mail.SMTP hiding (Ok, sendMail)
 import Network.Mail.Mime (Mail)
-import Text.JSON
-import Data.Maybe
+import Text.JSON (JSValue)
+import Data.Maybe (mapMaybe)
 import Data.Text (pack, unpack, intercalate)
 import Data.DateTime (fromSeconds, formatDateTime, DateTime)
 import qualified Data.Text.Lazy
-import Control.Monad
-import System.Environment
+import System.Environment (getArgs)
+import JsonParsing (httpGetJson, getByKey,
+                    fromJSRational, fromJSString', fromJSArray)
 
 newtype Latitude = Latitude Double deriving Show
 newtype Longitude = Longitude Double deriving Show
@@ -19,48 +19,6 @@ data Location = Location (Latitude, Longitude)
 
 london :: Location
 london = Location (Latitude 51.5265, Longitude 0.0825)
-
-httpGet :: URLString -> IO (CurlCode, String)
-httpGet url = curlGetString url []
-
--- get JSON, returning Nothing for any failures
--- based on http://www.amateurtopologist.com/blog/2010/11/05/a-haskell-newbies-guide-to-text-json/
-httpGetJson :: JSON a => URLString -> IO (Maybe a)
-httpGetJson url = do
-  (code, result) <- httpGet url
-  case (decode result) of
-    Ok object -> return (Just object)
-    Error s -> return Nothing
-
--- get the value in a JSON object that has this key
-getByKey :: String -> JSValue -> Maybe JSValue
-getByKey key (JSObject obj) = lookup key (fromJSObject obj)
-getByKey _ _ = Nothing
-
--- TODO: why can't I write `fromJust . getByKey`?
-getByKey' key obj = fromJust $ getByKey key obj
-
--- get the value in a JSON array at this index
-getByIndex :: Int -> JSValue -> Maybe JSValue
-getByIndex index (JSArray arr) = return (arr !! index)
-getByIndex _ _ = Nothing
-
--- map over this JS Array, if it's an array
-jsArrayMap :: (JSValue -> Maybe JSValue) -> JSValue -> JSValue
-jsArrayMap f (JSArray arr) = JSArray (mapMaybe f arr)
-jsArrayMap _ jsValue = jsValue
-
-fromJSArray :: JSValue -> Maybe [JSValue]
-fromJSArray (JSArray arr) = Just arr
-fromJSArray _ = Nothing
-
--- fromJSString :: JSValue -> Maybe 
-fromJSString' (JSString s) = Just $ fromJSString s
-fromJSString' _ = Nothing
-
-fromJSRational :: JSValue -> Maybe Double
-fromJSRational (JSRational _ ratio) = Just $ fromRational ratio
-fromJSRational _ = Nothing
 
 -- doesn't use scientific notation
 showDouble :: Double -> String
---
+++ JsonParsing.hs
@@ -0,0 +1,53 @@
+module JsonParsing where
+
+import Data.Maybe (fromJust, mapMaybe)
+import Network.Curl (curlGetString, CurlCode, URLString)
+import Text.JSON (JSON, decode, Result(Ok, Error),
+                  JSValue(JSArray, JSObject, JSString, JSRational),
+                  fromJSObject, fromJSString)
+
+
+httpGet :: URLString -> IO (CurlCode, String)
+httpGet url = curlGetString url []
+
+-- get JSON, returning Nothing for any failures
+-- based on http://www.amateurtopologist.com/blog/2010/11/05/a-haskell-newbies-guide-to-text-json/
+httpGetJson :: JSON a => URLString -> IO (Maybe a)
+httpGetJson url = do
+  (code, result) <- httpGet url
+  case (decode result) of
+    Ok object -> return (Just object)
+    Error s -> return Nothing
+
+-- get the value in a JSON object that has this key
+getByKey :: String -> JSValue -> Maybe JSValue
+getByKey key (JSObject obj) = lookup key (fromJSObject obj)
+getByKey _ _ = Nothing
+
+-- TODO: why can't I write `fromJust . getByKey`?
+getByKey' key obj = fromJust $ getByKey key obj
+
+-- get the value in a JSON array at this index
+getByIndex :: Int -> JSValue -> Maybe JSValue
+getByIndex index (JSArray arr) = return (arr !! index)
+getByIndex _ _ = Nothing
+
+-- map over this JS Array, if it's an array
+jsArrayMap :: (JSValue -> Maybe JSValue) -> JSValue -> JSValue
+jsArrayMap f (JSArray arr) = JSArray (mapMaybe f arr)
+jsArrayMap _ jsValue = jsValue
+
+fromJSArray :: JSValue -> Maybe [JSValue]
+fromJSArray (JSArray arr) = Just arr
+fromJSArray _ = Nothing
+
+-- fromJSString :: JSValue -> Maybe 
+fromJSString' (JSString s) = Just $ fromJSString s
+fromJSString' _ = Nothing
+
+fromJSRational :: JSValue -> Maybe Double
+fromJSRational (JSRational _ ratio) = Just $ fromRational ratio
+fromJSRational _ = Nothing
+
+
+
